8da5ac27f240fb6b6bad318c455364cb26bd4c33
Mike Newell <mike@iwearshorts.com>
2013-03-10 21:30:51 -0700

    adding lightbox

---
+++ js/choices.js
@@ -0,0 +1,166 @@
+var Choices = Class.extend({
+    
+    init: function(){
+        
+        // events
+        $('.video').on('dragstart', function(evt, ui) {
+
+            var $this = $(this);
+
+            if(!ui.helper.data('dropped')) {
+
+//                            if($this.parent().hasClass('choiceDrop')) {
+//                                var parentID = $this.data('key');
+//                                var elem = $this.detach();               
+//                                $('#' + parentID).html(elem);
+//                            }
+
+                var $parent = $this.parent('.videoContainer');
+
+                $parent.addClass('empty');
+
+                var tile = $this.find('.videoTile'),
+                    thumb = $this.find('.videoThumb');
+
+                tile.css({
+                    width: '40px',
+                    height: '40px',
+                    opacity: 0
+                });
+
+                thumb.css({
+                    width: '40px',
+                    height: '40px',
+                    opacity: 1
+                });
+
+                $this.css({
+                    width: '40px',
+                    height: '40px',
+                });
+            }
+        });
+
+        $('.video').on('dragstop', function(evt, ui) {
+
+            var $this = $(this);
+
+            if(!ui.helper.data('dropped')) {
+
+//                            if($this.parent().hasClass('choiceDrop')) {
+//                                var parentID = $this.data('key');
+//                                var elem = $this.detach();               
+//                                $('#' + parentID).html(elem);
+//                            }
+
+                var $parent = $this.parent('.videoContainer');
+
+                $parent.removeClass('empty');
+
+                var tile = $this.find('.videoTile'),
+                    thumb = $this.find('.videoThumb');
+
+                tile.css({
+                    width: '150px',
+                    height: '150px',
+                    opacity: 1
+                });
+
+                thumb.css({
+                    width: '150px',
+                    height: '150px',
+                    opacity: 0
+                });
+
+                $this.css({
+                    width: '150px',
+                    height: '150px'
+                });
+
+            } else {
+                var o = $(ui.helper.data('dropElem')).offset();
+                $(this).offset(o);
+            }
+        });
+        
+        this.setDraggable();
+        this.setDroppable();
+        
+    },
+            
+    /**
+     * Set the draggable element
+     * 
+     * @return {void} Nothing
+     * */
+     
+     setDraggable: function() {
+        $('.video').draggable({
+
+            // this is a neat little trick to get anything other than a drop to revert
+            revert: function(evt, ui) {
+
+                $(this).data('uiDraggable').originalPosition = {
+                    top: 0,
+                    left: 0,
+                    'z-index': 20
+                };
+
+                if($(this).parent().hasClass('choiceDrop') && !evt) {
+                    var o = $(this).parent('.choiceDrop').offset();
+                    var parentID = $(this).data('key');
+                    var elem = $(this).detach();               
+                    $('#' + parentID).html(elem);
+
+                    $(this).data('uiDraggable').currentPosition = o;
+                }
+
+                return !evt;
+            },
+            drag: function(evt, ui) {
+                $(this).css({
+                    'z-index': 30
+                });
+            },
+            cursorAt: {
+                left: 30,
+                top: 30
+            },
+            snap: '.choiceDrop',
+            snapMode: 'inner'
+        });       
+     },
+     
+     
+     /**
+     * Set the droppable element
+     * 
+     * @return {void} Nothing
+     * */
+     
+     setDroppable: function() {
+        $( ".choiceDrop" ).droppable({
+            drop: function( evt, ui ) {
+                ui.draggable.data('dropped', true);
+                ui.draggable.data('dropElem', this);
+
+                var elem = ui.draggable.detach();                            
+                $(this).html(elem);
+
+                $(this).droppable('option', 'accept', ui.draggable);
+            },
+            out: function(evt, ui) {
+                ui.draggable.data('dropped', false);
+                ui.draggable.data('dropElem', null);
+
+                // find the original parent of the item
+        //                            var p = ui.draggable.data('key');
+        //                            var elem = ui.draggable.detach();                            
+        //                            $('#' + p).html(elem);
+
+                $(this).droppable('option', 'accept', '.video');
+            }
+        });
+     }
+      
+});
--- select/index.php
+++ select/index.php
@@ -1,3 +1,71 @@
+<?php
+    $lightboxArr = array(
+        1 => 'http://player.vimeo.com/video/61393506?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        2 => 'http://player.vimeo.com/video/61393505?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        3 => 'http://player.vimeo.com/video/61393507?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        4 => array('images' => array(
+            '../images/path/to/images.jpg',
+            '../images/path/to/images.jpg',
+            '../images/path/to/images.jpg'
+        )),
+        5 => 'http://player.vimeo.com/video/61393323?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        6 => 'http://player.vimeo.com/video/61393321?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        7 => 'http://player.vimeo.com/video/61393320?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        8 => 'http://player.vimeo.com/video/61393319?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        9 => 'http://player.vimeo.com/video/61393318?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        10 => 'http://player.vimeo.com/video/61393145?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        11 => 'http://player.vimeo.com/video/61393144?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        12 => 'http://player.vimeo.com/video/61393142?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        13 => 'http://player.vimeo.com/video/61393139?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        14 => 'http://player.vimeo.com/video/61393136?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        15 => 'http://player.vimeo.com/video/61393007?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        16 => 'http://player.vimeo.com/video/61393006?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        17 => 'http://player.vimeo.com/video/61393005?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        18 => 'http://player.vimeo.com/video/61393004?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        19 => 'http://player.vimeo.com/video/61393001?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        20 => 'http://player.vimeo.com/video/61392327?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        21 => 'http://player.vimeo.com/video/61392326?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        22 => 'http://player.vimeo.com/video/61392324?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        23 => 'http://player.vimeo.com/video/61392323?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        24 => 'http://player.vimeo.com/video/61392322?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        25 => 'http://player.vimeo.com/video/61391864?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        26 => 'http://player.vimeo.com/video/61391863?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        27 => 'http://player.vimeo.com/video/61391862?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        28 => 'http://player.vimeo.com/video/61391861?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        29 => 'http://player.vimeo.com/video/61391859?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        30 => 'http://player.vimeo.com/video/61391398?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        31 => 'http://player.vimeo.com/video/61391397?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        32 => 'http://player.vimeo.com/video/61391396?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        33 => 'http://player.vimeo.com/video/61391715?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        34 => 'http://player.vimeo.com/video/61391393?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        35 => 'http://player.vimeo.com/video/61391144?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        36 => 'http://player.vimeo.com/video/61392869?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        37 => 'http://player.vimeo.com/video/61391143?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        38 => 'http://player.vimeo.com/video/61391139?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        39 => 'http://player.vimeo.com/video/61391138?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        40 => 'http://player.vimeo.com/video/61391136?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        41 => 'http://player.vimeo.com/video/61392867?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        42 => array('images' => array(
+            '../images/path/to/images.jpg',
+            '../images/path/to/images.jpg',
+            '../images/path/to/images.jpg'
+        )),
+        43 => 'http://player.vimeo.com/video/61385504?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        44 => 'http://player.vimeo.com/video/61394273?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        45 => 'http://player.vimeo.com/video/61385498?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        46 => 'http://player.vimeo.com/video/61385494?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        47 => 'http://player.vimeo.com/video/61385220?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        48 => 'http://player.vimeo.com/video/61385218?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        49 => 'http://player.vimeo.com/video/61392868?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        50 => 'http://player.vimeo.com/video/61385217?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+        51 => array('images' => array(
+            '../images/path/to/images.jpg',
+            '../images/path/to/images.jpg',
+            '../images/path/to/images.jpg'
+        )),
+        52 => 'http://player.vimeo.com/video/61385215?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff',
+    )
+?>
 <!-- 
     Author: Mike Newell © 2012
 -->
@@ -27,7 +95,10 @@ and open the template in the editor.
                     <div class="nav"></div>
                 </header>
                 <section>
-                    <header><h1>Help Select 10 for 30</h1></header>
+                    <header><h1>Start Voting</h1></header>
+                    <article>
+                        <p>Click to video any of the campaigns. Drag your favorites into your Top 10 list on the right. You can re-order your ranking before you submit. There is space for “write-in” votes. <strong>Votes are due by 5 p.m. PDT, Friday, March 22.</strong></p>
+                    </article>
                     <article>
                         <div class="videos">
                             
@@ -51,6 +122,7 @@ and open the template in the editor.
                                 <div class="video" data-key="$id">
                                     <img class="videoThumb" src="../images/40x40/$i.jpg" />
                                     <img class="videoTile" src="../images/150x150/$i.jpg" />
+                                    <a class="videoLink fancybox-media" href="http://player.vimeo.com/video/61393506?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff"></a>
                                 </div>
                             </div>
 BLOCK;
@@ -97,153 +169,51 @@ BLOCK;
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/jquery-ui.min.js"></script>
+        
+        <!-- Add mousewheel plugin (this is optional) -->
+	<script type="text/javascript" src="../lib/jquery.mousewheel-3.0.6.pack.js"></script>
+
+	<!-- Add fancyBox main JS and CSS files -->
+	<script type="text/javascript" src="../source/jquery.fancybox.js?v=2.1.4"></script>
+	<link rel="stylesheet" type="text/css" href="../source/jquery.fancybox.css?v=2.1.4" media="screen" />
+
+	<!-- Add Button helper (this is optional) -->
+	<link rel="stylesheet" type="text/css" href="../source/helpers/jquery.fancybox-buttons.css?v=1.0.5" />
+	<script type="text/javascript" src="../source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+
+	<!-- Add Thumbnail helper (this is optional) -->
+	<link rel="stylesheet" type="text/css" href="../source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" />
+	<script type="text/javascript" src="../source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+
+	<!-- Add Media helper (this is optional) -->
+	<script type="text/javascript" src="../source/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
+        
         <script src="../js/cufon.js"></script>
         <script src="../js/berthold.js"></script>
+        <script src="../js/class.js"></script>
+        <script src="../js/choices.js"></script>
         <script>
             (function($) {
                 $(window).load(function() {
                     
-                    // events
-                    $('.video').on('dragstart', function(evt, ui) {
-                        
-                        var $this = $(this);
-                        
-                        if(!ui.helper.data('dropped')) {
-                            
-//                            if($this.parent().hasClass('choiceDrop')) {
-//                                var parentID = $this.data('key');
-//                                var elem = $this.detach();               
-//                                $('#' + parentID).html(elem);
-//                            }
-                            
-                            var $parent = $this.parent('.videoContainer');
-
-                            $parent.addClass('empty');
-
-                            var tile = $this.find('.videoTile'),
-                                thumb = $this.find('.videoThumb');
-                                
-                            tile.css({
-                                width: '40px',
-                                height: '40px',
-                                opacity: 0
-                            });
-
-                            thumb.css({
-                                width: '40px',
-                                height: '40px',
-                                opacity: 1
-                            });
-                            
-                            $this.css({
-                                width: '40px',
-                                height: '40px',
-                            });
-                        }
-                    });
-                    
-                    $('.video').on('dragstop', function(evt, ui) {
-                        
-                        var $this = $(this);
-                        
-                        if(!ui.helper.data('dropped')) {
-                            
-//                            if($this.parent().hasClass('choiceDrop')) {
-//                                var parentID = $this.data('key');
-//                                var elem = $this.detach();               
-//                                $('#' + parentID).html(elem);
-//                            }
-                            
-                            var $parent = $this.parent('.videoContainer');
-
-                            $parent.removeClass('empty');
-
-                            var tile = $this.find('.videoTile'),
-                                thumb = $this.find('.videoThumb');
-
-                            tile.css({
-                                width: '150px',
-                                height: '150px',
-                                opacity: 1
-                            });
-
-                            thumb.css({
-                                width: '150px',
-                                height: '150px',
-                                opacity: 0
-                            });
-
-                            $this.css({
-                                width: '150px',
-                                height: '150px'
-                            });
-                            
-                        } else {
-                            var o = $(ui.helper.data('dropElem')).offset();
-                            $(this).offset(o);
-                        }
-                    });
-                    
-                    // set elements in ui
-                    $('.video').draggable({
-                        
-                        // this is a neat little trick to get anything other than a drop to revert
-                        revert: function(evt, ui) {
-                            
-                            $(this).data('uiDraggable').originalPosition = {
-                                top: 0,
-                                left: 0,
-                                'z-index': 20
-                            };
-                            
-                            if($(this).parent().hasClass('choiceDrop') && !evt) {
-                                var o = $(this).parent('.choiceDrop').offset();
-                                var parentID = $(this).data('key');
-                                var elem = $(this).detach();               
-                                $('#' + parentID).html(elem);
-                                
-                                $(this).data('uiDraggable').currentPosition = o;
-                            }
-                            
-                            return !evt;
-                        },
-                        drag: function(evt, ui) {
-                            $(this).css({
-                                'z-index': 30
-                            });
-                        },
-                        cursorAt: {
-                            left: 30,
-                            top: 30
-                        },
-                        snap: '.choiceDrop',
-                        snapMode: 'inner'
-                    });
-                    
-                    $( ".choiceDrop" ).droppable({
-                        drop: function( evt, ui ) {
-                            ui.draggable.data('dropped', true);
-                            ui.draggable.data('dropElem', this);
-                            
-                            var elem = ui.draggable.detach();                            
-                            $(this).html(elem);
-
-                            $(this).droppable('option', 'accept', ui.draggable);
-                        },
-                        out: function(evt, ui) {
-                            ui.draggable.data('dropped', false);
-                            ui.draggable.data('dropElem', null);
-                            
-                            // find the original parent of the item
-//                            var p = ui.draggable.data('key');
-//                            var elem = ui.draggable.detach();                            
-//                            $('#' + p).html(elem);
-                            
-                            $(this).droppable('option', 'accept', '.video');
-                        }
-                    });
+                    var c = new Choices();
                     
                     Cufon.replace('h1, p, h2');
+                    
+                    $('.fancybox-media')
+                    .attr('rel', 'media-gallery')
+                    .fancybox({
+                        openEffect : 'none',
+                        closeEffect : 'none',
+                        prevEffect : 'none',
+                        nextEffect : 'none',
+
+                        arrows : false,
+                        helpers : {
+                            media : {},
+                            buttons : {}
+                        }
+                    });
                     
                 });
             })(jQuery);
